1e13bae2112e5da8e7708c537ac40d11f0b7b9b1
Rawaa Rajab <rawaa.rajab@gmail.com>
2019-04-22 15:41:36 +0300

    Update Problem003.java

--- ProjectEuler/src/ProjectEuler/Problem003.java
+++ ProjectEuler/src/ProjectEuler/Problem003.java
@@ -24,4 +24,8 @@ public class Problem003 {
             }
         }
     }
+
+    boolean run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
